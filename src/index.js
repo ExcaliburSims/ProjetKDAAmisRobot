5f5ce26fe7d79186e169e28c98bf20c460c8f782
sim's zilu <ximeonzilu@gmail.com>
2021-05-03 11:55:43 +0100

    ajout des fichiers

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles.css";
+import "./App.css";
+//librairie css tachyons
+import "tachyons";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
